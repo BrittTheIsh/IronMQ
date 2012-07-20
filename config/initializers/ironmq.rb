7f87b95d9d66a83793b7a78b590f7c27e9627e6c
Britt Mileshosky <bmileshosky@engineyard.com>
2012-07-20 16:36:29 -0600

    Updating with potentially correct ENV names.

--- config/initializers/ironmq.rb
+++ config/initializers/ironmq.rb
@@ -1,4 +1,4 @@
 IRON_MQ = IronMQ::Client.new(
-   token:        EY::Config.get(:iron_mq, 'IRON_WORKER_PROJECT_ID'),
-   project_id:   EY::Config.get(:iron_mq, 'IRON_WORKER_TOKEN')
+   token:        EY::Config.get(:iron_worker, 'IRON_WORKER_PROJECT_ID'),
+   project_id:   EY::Config.get(:iron_worker, 'IRON_WORKER_TOKEN')
 )
