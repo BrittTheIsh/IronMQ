b573447228cd4eb9c2ab616488630cd4fa014acf
Britt Mileshosky <bmileshosky@engineyard.com>
2012-07-20 15:36:38 -0600

    Removing unnecessary config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,6 @@ IronMQ::Application.configure do
   
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
